67bd9fd0227583d88027504145a743c4a69f185e
Ajeeth <tajeeth@yahoo.com>
2022-03-28 11:56:19 +0530

    CreateProduct functionality added

--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Input, Button, Space, Typography, Row, Col, Select, Menu, Card, Form, Radio } from "antd";
+import { DatePicker,Input, Button, Space, Typography, Row, Col, Select, Menu, Card, Form, Radio } from "antd";
 import { } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import '../Style/CreateProduct.css'
@@ -13,7 +13,11 @@ class CreateProduct extends React.Component {
             productWeight: '',
             isOrganic: 'yes',
             Time: '',
-            location: null
+            location: null,
+            isProductID: true,
+            isProductName: true,
+            isProductWeight: true,
+            isLocation: true
         }
     }
 
@@ -25,8 +29,56 @@ class CreateProduct extends React.Component {
             productWeight: '',
             isOrganic: 'yes',
             Time: '',
-            location: null
+            location: null,
         })
+        this.errorClear();
+    }
+
+    errorClear=()=>{
+        this.setState({
+            isProductID: true,
+            isProductName: true,
+            isProductWeight: true,
+            isLocation: true
+        })
+    }
+
+    createProduct = () => {
+        this.errorClear();
+        debugger
+        if (this.state.productID == '' || this.state.productID == null || this.state.productID == undefined) {
+            this.setState({
+                isProductID: false
+            })
+        }
+
+        if (this.state.productName == '' || this.state.productName == null || this.state.productName == undefined) {
+            this.setState({
+                isProductName: false
+            })
+        }
+
+        if (this.state.productWeight == '' || this.state.productWeight == null || this.state.productWeight == undefined) {
+            this.setState({
+                isProductWeight: false
+            })
+        }
+
+        if (this.state.location == '' || this.state.location == null || this.state.location == undefined) {
+            this.setState({
+                isLocation: false
+            })
+        }
+
+        if(this.state.isProductID && this.state.isProductName && this.state.isProductWeight && this.state.isLocation)
+        {
+            var cDate = new Date();
+
+            var data=this.state.productID+'-'+this.state.productName+'-'+this.state.productWeight+'-'+this.state.isOrganic+'-'+this.state.location+'-'+this.state.Time+'Farmer'
+
+            fetch('http://localhost:8001/add_produce/'+data).then(res=>console.log(res));
+            console.log(data)
+        }
     }
 
     render() {
@@ -36,32 +88,35 @@ class CreateProduct extends React.Component {
                 <Space direction="vertical">
                     <table className="create-input-table">
                         <tr>
-                            <td>Product ID:</td>
-                            <td><Input id="com-input" onChange={x => {
+                            <td className="label-col">Product ID:</td>
+                            <td className="input-col"><Input id="com-input" onChange={x => {
                                 this.setState({
                                     productID: x.target.value
                                 })
                             }} value={this.state.productID} /></td>
+                            {!this.state.isProductID && <td className="error-col">please enter Product Id!</td>}
                         </tr>
                         <tr>
-                            <td>Product Name:</td>
-                            <td><Input id="com-input" onChange={x => {
+                            <td className="label-col">Product Name:</td>
+                            <td className="input-col"><Input id="com-input" onChange={x => {
                                 this.setState({
                                     productName: x.target.value
                                 })
                             }} value={this.state.productName} /></td>
+                            {!this.state.isProductName && <td className="error-col">please enter Product Name!</td>}
                         </tr>
                         <tr>
-                            <td>Product Weight:</td>
-                            <td><Input type="number" id="com-input" onChange={x => {
+                            <td className="label-col">Product Weight:</td>
+                            <td className="input-col"><Input type="number" id="com-input" onChange={x => {
                                 this.setState({
                                     productWeight: x.target.value
                                 })
                             }} value={this.state.productWeight} /></td>
+                            {!this.state.isProductWeight && <td className="error-col">please enter Product Weight!</td>}
                         </tr>
                         <tr>
-                            <td>Is Organic:</td>
-                            <td><Radio.Group defaultValue="yes" value={this.state.isOrganic} onChange={x => {
+                            <td className="label-col">Is Organic:</td>
+                            <td className="input-col"><Radio.Group defaultValue="yes" value={this.state.isOrganic} onChange={x => {
                                 this.setState({
                                     isOrganic: x.target.value
                                 })
@@ -72,15 +127,19 @@ class CreateProduct extends React.Component {
                         </tr>
 
                         <tr>
-                            <td>Time:</td>
-                            <td><Input id="com-input"  disabled="true" value={this.state.Time} /></td>
+                            <td className="label-col">Time:</td>
+                            <td className="input-col"><DatePicker id="com-input" value={this.state.Time} onChange={x=>{
+                                this.setState({
+                                    Time:x.value
+                                })
+                            }} /></td>
                         </tr>
 
                         <tr>
-                            <td>Location:</td>
-                            <td><Select className="location-select" placeholder="select location" value={this.state.location} onChange={x => {
+                            <td className="label-col">Location:</td>
+                            <td className="input-col"><Select className="location-select" placeholder="select location" value={this.state.location} onChange={x => {
                                 this.setState({
-                                    location: x.value
+                                    location: x
                                 })
                             }}>
                                 <Option value="Chennai">
@@ -90,9 +149,10 @@ class CreateProduct extends React.Component {
                                     Vellore
                                 </Option>
                             </Select></td>
+                            {!this.state.isLocation && <td className="error-col">please select Location!</td>}
                         </tr>
                     </table>
-                    <Button type="primary" id="com-button">Create</Button>
+                    <Button type="primary" id="com-button" onClick={this.createProduct}>Create</Button>
                     <Button type="primary" id="com-button" danger ghost onClick={this.createClear}>Clear</Button>
                 </Space>
             </div>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,35 +9,58 @@ import TransferProduct from "./TransferProduct";
 class Dashboard extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            cPMenu: true,
+            tPMenu: false,
+            gPDMenu: false
+        }
+    }
+
+    menuClick = (e) => {
+        debugger
+        if (e.key === "createProduct-menu") {
+            this.setState({
+                cPMenu: true,
+                tPMenu: false,
+                gPDMenu: false
+            })
+        }
+        if (e.key === "transferProduct-menu") {
+            this.setState({
+                cPMenu: false,
+                tPMenu: true,
+                gPDMenu: false
+            })
+        }
     }
 
     render() {
         return (
             <div className="farmerDashboard-wrapper">
                 <Row span={24}>
-                <Col span={20}>
-                <Typography.Title style={{"text-align":"center"}} level={2}>Farmer Dashboard</Typography.Title>
-                </Col>
-                <Col span={4}>
-                <Card className="Dashboard-wrapper" bordered={false}>
-                    <p>Ajeeth</p>
-                    <p>Farmer</p>
-                </Card>
-                </Col>
+                    <Col span={20}>
+                        <Typography.Title style={{ "text-align": "center" }} level={2}>Farmer Dashboard</Typography.Title>
+                    </Col>
+                    <Col span={4}>
+                        <Card className="Dashboard-wrapper" bordered={false}>
+                            <p>Ajeeth</p>
+                            <p>Farmer</p>
+                        </Card>
+                    </Col>
                 </Row>
                 <Menu mode="horizontal">
-                    <Menu.Item>
+                    <Menu.Item key="createProduct-menu" onClick={this.menuClick}>
                         Create product
                     </Menu.Item>
-                    <Menu.Item>
+                    <Menu.Item key="transferProduct-menu" onClick={this.menuClick}>
                         Transfer product
                     </Menu.Item>
-                    <Menu.Item>
+                    <Menu.Item key="getProductDetails-menu" onClick={this.menuClick}>
                         Get product details
                     </Menu.Item>
                 </Menu>
-                {/* <CreateProduct/> */}
-                <TransferProduct/>
+                {this.state.cPMenu &&<CreateProduct/>}
+                {this.state.tPMenu &&<TransferProduct />}
             </div>
         )
     }
